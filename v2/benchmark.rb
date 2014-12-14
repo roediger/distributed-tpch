70d4461bb97ac665970c9c63a8a3f6952b8679fa
Erik Muttersbach <erik@muttersbach.net>
2014-12-14 10:00:31 +0100

    Updated Benchmark.rb

--- v2/benchmark.rb
+++ v2/benchmark.rb
@@ -1,10 +1,12 @@
 #!/usr/bin/ruby
 
 require 'optparse'
+require 'date'
 
 options = {
 	:scale_factor => 1,
-	:benchmarks => [:impala, :hive, :presto]	
+	:benchmarks => [:impala, :hive, :presto],	
+	:queries => 1..22
 }
 
 configuration = {
@@ -31,6 +33,9 @@ OptionParser.new do |opts|
 	opts.on("-b", "--benchmarks BENCHMARKS", "Select Benchmarks to run") do |b|
   		options[:benchmarks] = b.split(',').map{|b| b.to_sym}
   	end
+	opts.on("-q", "--queries QUERIES", "Select the queries to run") do |q|
+		options[:queries] = q.split(',').map{|q| q.to_i}
+	end
 	opts.on("-f", "--scale-factor SCALE-FACTOR", "Select the scale factor of the TPC-H data set") do |sf|
 		options[:scale_factor] = Integer(sf)
 	end
@@ -46,29 +51,42 @@ OptionParser.new do |opts|
     end
 end.parse!
 
-### Update Configuration based on parsed options ###
-
-configuration[:presto][:before_query_sql] = "USE CATALOG tpch; USE SCHEMA sf#{options[:scale_factor]};"
-
 
 ### Run the queries ###
 
-for i in 1..22 do
-	print "Running query #{i}\n"
-	options[:benchmarks].each do |benchmark|
-		print " -> #{benchmark}\n"
+File.open("benchmark.#{DateTime.now().to_time.to_i}.csv", 'w') do |f|
+  options[:benchmarks].each do |benchmark|
+    f.write "#{benchmark},"
+    
+    for i in options[:queries] do
+  		print " -> #{benchmark}\n"
 
-		prepare_file = "#{benchmark}/prepare/q#{i.to_s.rjust(2, '0')}.sql"
-		query_file = "#{benchmark}/queries/q#{i.to_s.rjust(2, '0')}.sql"
-		query = File.read(query_file)
+  		prepare_file = "#{benchmark}/prepare/q#{i.to_s.rjust(2, '0')}.sql"
+  		query_file = "#{benchmark}/queries/q#{i.to_s.rjust(2, '0')}.sql"
+  		query = File.read(query_file)
 
-		if File.exists? prepare_file  
-			`#{configuration[benchmark][:cmd]} #{configuration[benchmark][:query_file_option]} #{prepare_file}`
-		end	
+  		if File.exists? prepare_file  
+  			`#{configuration[benchmark][:cmd]} #{configuration[benchmark][:query_file_option]} #{prepare_file}`
+  		end
 
-		`#{configuration[benchmark][:cmd]} #{configuration[benchmark][:query_file_option]} "#{query_file}"`
-	end 		
+  		additional_options = ""
+  		if benchmark == :presto
+  			additional_options = "--catalog tpch --schema sf#{options[:scale_factor]}"	
+  		end
+      
+      start = Time.now().to_f
+  		out = `#{configuration[benchmark][:cmd]} #{additional_options} #{configuration[benchmark][:query_file_option]} "#{query_file}" 2>&1`
+      d = Time.now().to_f - start
+      
+      f.write "#{d}"+(i == options[:queries].last ? '\n' : ',')
+  	end 		
+  end
+  
+  File.unlink('benchmark.csv') if File.exists?('benchmark.csv')
+  p "ln -s #{f} ./benchmark.csv"
 end
+
+
 
 #p options
 #p configuration
