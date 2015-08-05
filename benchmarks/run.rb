0d5267856f77fe634657001243d2dab25d81c024
Wolf Roediger <roediger@in.tum.de>
2015-08-05 12:09:36 +0200

    measure runtime and I/O statistics

--- benchmarks/run.rb
+++ benchmarks/run.rb
@@ -18,10 +18,12 @@ options.queries = 1..22
 options.n = 1
 options.sf = 100
 options.verify = true
+options.profile = false
 options.drop_caches = false
 options.restart = false
-options.drop_caches_cmd = 'for i in `seq 11 16`;do ssh scyper$i "/usr/local/bin/flush_fs_caches"; done'
-options.verify_dir = '~/repositories/verify'
+options.drop_caches_cmd = "for i in `seq 11 16`; do ssh scyper$i \"/usr/local/bin/flush_fs_caches\"; done"
+options.verify_dir = "~/repositories/verify"
+options.statistics_cmd = "for i in `seq 11 16`; do ssh scyper$i \"awk '{print \\$3, \\$7}' /sys/class/block/sda/stat /sys/class/block/sdb/stat /sys/class/block/sdc/stat | cat - /sys/class/infiniband/mlx4_0/ports/1/counters/port_rcv_packets /sys/class/infiniband/mlx4_0/ports/1/counters/port_xmit_packets /sys/class/infiniband/mlx4_0/ports/1/counters/port_rcv_data /sys/class/infiniband/mlx4_0/ports/1/counters/port_xmit_data | xargs\"; done"
 
 OptionParser.new do |opts|
   opts.banner = "Usage: #{$0} [options]"
@@ -41,6 +43,9 @@ OptionParser.new do |opts|
   opts.on("-v", "--verify VERIFY-RESULTS", [:true, :false], "Whether the query results should be verified (default " + options.verify.to_s + ")") do |v|
     options.verify = (v == :true)
   end
+  opts.on("-p", "--profile PROFILE-IO", [:true, :false], "Whether the disk and network I/O should be profiled (default " + options.profile.to_s + ")") do |p|
+    options.profile = (p == :true)
+  end
   opts.on("-d", "--drop-caches DROP-CACHES", [:true, :false], "Whether FS caches should be dropped between runs (default " + options.drop_caches.to_s + ")") do |d|
     options.drop_caches = (d == :true)
   end
@@ -54,11 +59,33 @@ OptionParser.new do |opts|
   end
 end.parse!(ARGV)
 
+def gather_statistics cmd
+    # split statistics by host
+    statistics = []
+    `#{cmd} 2>&1 | tee -a run.log`.split("\n").each do |statistics_host|
+puts statistics_host.gsub(/ /, "\t")
+      statistics << statistics_host.split(' ').map(&:to_i)
+    end
+
+    # sum statistics of hosts
+    statistics.inject do |result, element|
+      element.each_with_index do |value, index|
+        result[index] += value
+      end
+      result
+    end
+end
+
+def geomean(array)
+  array.inject(1.0, :*) ** (1.0 / array.size)
+end
+
 puts "$configuration:"
 puts "\tBenchmarks:\t"+options.benchmarks.join(', ')
 puts "\t#Queries:\t"+options.queries.count.to_s
 puts "\tRuns:\t\t"+options.n.to_s
 puts "\tVerify results:\t"+options.verify.to_s
+puts "\tProfile I/O:\t"+options.profile.to_s
 puts "\tRestart:\t"+options.restart.to_s
 puts "\tDrop caches:\t"+options.drop_caches.to_s
 
@@ -78,13 +105,22 @@ File.open("run.csv", 'w') do |f|
     print "#{benchmark.name}\n"
     f.write "#{benchmark.name}\n"
 
+    # gather statistics
+    statistics_before = []
+    if (options.profile)
+      puts "gathering I/O stats..."
+      statistics_before = gather_statistics options.statistics_cmd
+    end
+
     # Run each query
+    runtimes = []
     for q in options[:queries] do
 
       # Write current run to console and file
       print "Q#{q}\t"
 
-      # Run each benchmark N times
+      # Run each query N times
+      query_runtimes = []
       for n in 1..options[:n] do
         `echo "Running Benchmark #{benchmark.name}, Query #{q}, Run #{n}" >> run.log`
 
@@ -102,6 +138,7 @@ File.open("run.csv", 'w') do |f|
         # the console
         if t
           print ".".green
+          query_runtimes << t
           t = t.is_a?(Array) ? t : [t]
           f.write(t.join("\t"))
         else
@@ -115,9 +152,32 @@ File.open("run.csv", 'w') do |f|
           print "\n"
         end
       end
+      if !query_runtimes.empty?
+        runtimes << query_runtimes.median
+      end
     end
+
+    runtime = runtimes.reduce(:+)
+    puts "time\t#{runtime.round(2)}"
+    puts "geomean\t#{geomean(runtimes).round(2)}"
+    puts "QpH\t#{(options[:queries].size*60.0*60.0/runtime).round(2)}"
 
     # verify results
     benchmark.verify
+
+    # gather statistics
+    if options.profile
+      puts "gathering I/O stats..."
+      statistics = []
+      (gather_statistics options.statistics_cmd).each_with_index do |value, index|
+        statistics[index] = value - statistics_before[index]
+      end
+      puts "disk read\t#{((statistics[0]+statistics[2]+statistics[4]).to_f/1000/1000).round(2)} GB"
+      puts "disk write\t#{((statistics[1]+statistics[3]+statistics[5].to_f)/1000/1000).round(2)} GB"
+      puts "packets recv\t#{(statistics[6].to_f/1000/1000).round(2)} million"
+      puts "packets sent\t#{(statistics[7].to_f/1000/1000).round(2)} million"
+      puts "data recv\t#{(statistics[8]*4.0/1000/1000/1000).round(2)} GB"
+      puts "data sent\t#{(statistics[9]*4.0/1000/1000/1000).round(2)} GB"
+    end
   end
 end
